4a2609391b0e9d39fcebd8ae2ce32f330365c6aa
diegoyellow <diegoyellow01@gmail.com>
2017-02-08 17:44:47 -0600

    a
    a

--- method.php
+++ method.php
@@ -10,13 +10,15 @@ $name = $_POST['name'];
 $messagex = $_POST['message'];
 $tel = $_POST['phone'];
 $cell = $_POST['cellphone'];
-$message = Swift_Message::newInstance('Mensaje Recibido')
-->setFrom(array("asesordiegoye@gmail.com"=>'Asesor de Productos Ye'))
-->setTo(array($mail=>"cliente"))
-->setBody('<div style="background-color:#D40042;font-size:30px;height:auto;width:100%">El asesor de productos Ye ha recibido su mensaje. En un período
-menor a 24 horas, el responderá su mensaje. En caso de no ser así, mandar
-un mensaje al número: 8123703188</div>', 'text/html');
-/*
+if (preg_match("/[a-zA-z0-9\.]+@[a-zA-z0-9]+\.[a-zA-z]+/", $mail)){
+  $message = Swift_Message::newInstance('Mensaje Recibido')
+  ->setFrom(array("asesordiegoye@gmail.com"=>'Asesor de Productos Ye'))
+  ->setTo(array($mail=>"cliente"))
+  ->setBody('<div style="background-color:#D40042;font-size:30px;height:auto;width:100%">El asesor de productos Ye ha recibido su mensaje. En un período
+  menor a 24 horas, el responderá su mensaje. En caso de no ser así, mandar
+  un mensaje al número: 8123703188</div>', 'text/html');
+  $mailer->send($message);
+}
 $newTransport = Swift_SmtpTransport::newInstance('ssl://smtp.gmail.com', 465)
   ->setUsername('asesordiegoye@gmail.com')
   ->setPassword('procesadordecorreos');
@@ -25,23 +27,21 @@ $newMailer = Swift_Mailer::newInstance($newTransport);
 
 $newMessage = Swift_Message::newInstance('Cliente solicita información')
 ->setFrom(array('asesordiegoye@gmail.com'=>'Asesor de Productos Ye'))
-->setTo(array('diego13132011@me.com'=>'A'))
-->setBody('Un cliente llamado' . $name . "solicita informacion de los productos, dejo este mensaje: <br>" . $message . "<br>
+->setTo(array('doad19590@gmail.com'=>'A'))
+->setBody('Un cliente llamado ' . " " . $name . " " . " solicita informacion de los productos, dejo este mensaje: <br>" . $messagex . "<br>
 su numero de telefono es: " . $tel . "<br>Su celular es: " . $cell . "<br>Su correo es: " . $mail  , "text/html");
 $newMailer->send($newMessage);
-*/
-$newMailer = Swift_Mailer::newInstance($transport);
+
+/*$newMailer = Swift_Mailer::newInstance($transport);
 $newMessage = Swift_Message::newInstance('Cliente solicita información')
 ->setFrom(array('asesordiegoye@gmail.com'=>'Asesor de Productos Ye'))
-->setTo(array('diego13132011@me.com'=>'A'))
+->setTo(array('doad19590@gmail.com'=>'A'))
 ->setBody('Un cliente llamado' . $name . "solicita informacion de los productos, dejo este mensaje: <br>" . $messagex . "<br>
-su numero de telefono es: " . $tel . "<br>Su celular es: " . $cell . "<br>Su correo es: " . $mail  , "text/html");
-if ($mailer->send($message)){
-    echo "Mensaje enviad";
-    echo $messagex;
-    $newMailer->send($newMessage);
+su numero de telefono es: " . $tel . "<br>Su celular es: " . $cell . "<br>Su correo es: " . $mail  , "text/html");*/
+echo "Mensaje enviado";
+echo "<script>location.href='https://diegoyellow.github.io/index.html';</script>";
 
-}
+
 
 
 ?>
